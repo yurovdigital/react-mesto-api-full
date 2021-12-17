3065481dc4cda652703d6dc2994e5640f0cd6ca4
YD <yurovdigital@gmail.com>
2021-12-17 18:10:30 +0300

    fix CORS

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,20 +4,19 @@ const allowedCors = [
   'https://yurov.mesto.nomoredomains.rocks',
   'http://yurov.mesto.nomoredomains.rocks',
   'localhost:3000',
+  'http://localhost:3000',
 ]
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers
+  const { method } = req
+  const requestHeaders = req.headers['access-control-request-headers']
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin)
 
     res.header('Access-Control-Allow-Credentials', 'true')
   }
-
-  const { method } = req
-
-  const requestHeaders = req.headers['access-control-request-headers']
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS)
